8ceb6c10908da06a9fc3ba71b8822d0eb5e36500
Ivan <ivancizik@gmail.com>
2019-06-23 14:39:14 +0200

    Added advance search

--- xl_viewer.py
+++ xl_viewer.py
@@ -98,19 +98,60 @@ while True:  # making a loop
 
     # Search
     if xl_input == "2":
+        print(
+            '''
+            Use the following search functions:
+            string      = check is searched item is a match
+            *string     = check is searched item ends with string
+            string*     = check is searched item starts with string
+            *string*    = check is searched item contains with string
+            -h          = add headers to serach resutls (first row is header)
+            '''
+            )
+
         xl_search_term = input("Search for: ")
         xl_search_column = input("Select column: ")
+        offset = 0
 
         wb.create_sheet("Results")
         sheet_result = wb["Results"]
+
+        if xl_search_term[-2:] == "-h":
+            counter = 0
+            offset = 3
+
+            for row in sheet.iter_rows(1):
+                counter = counter + 1
+                if counter == 1:
+                    values = (c.value for c in row)
+                    sheet_result.append(values)
+                if counter == 1:
+                    break
 
         for row in sheet.iter_rows():
             xl_compare = str(row[column_index_from_string(xl_search_column[0]) - 1].value)
 
             if xl_compare == "None":
                 xl_compare = "something"
+
+            if xl_search_term[-2:] == "-h":
+                offset = 3
+
+            if xl_search_term[0] == "*" and xl_search_term[-1 - offset] == "*":
+                search_results = xl_search_term.split("*")[1].lower() in xl_compare.lower()
+
+            else:
+                search_results = xl_search_term.split(" -")[0].lower() == xl_compare.lower()
+
+            if xl_search_term[0] == "*":
+                search_results = xl_compare.lower().endswith(xl_search_term.split("*")[1].split(" -")[0].lower())
+
+            if xl_search_term[-1 - offset] == "*":
+                search_results = xl_compare.lower().startswith(xl_search_term.split("*")[0].lower())
+
+            offset = 0 # reset to 0 in in loop
                 
-            if xl_search_term.lower() in xl_compare.lower():
+            if search_results == True:
                 sheet_result.append((cell.value for cell in row))
         
         sheet = wb["Results"]
